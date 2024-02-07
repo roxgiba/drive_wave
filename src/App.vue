cbfbb369c9540dfe390b2f177f71b1e1eb703f50
roxgiba <rox.giba@gmail.com>
2024-02-07 18:10:09 +0100

    components created correctly

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,77 @@
     <h1 className="text-3xl font-bold md:text-5xl bg-[#6bbbae] p-5 text-white">
       Welcome to <span class="italic">DriveWave</span>
     </h1>
+    <h4 className="text-sm md:text-lg">*This is a tech challenge for RoadSurfer</h4>
   </header>
 
   <main>
-    <h4 className="text-sm md:text-lg">*This is a tech challenge for RoadSurfer</h4>
-    <auto-complete @station-selected="handleStationSelected" className="mt-36" />
+    <div class="text-center m-5">
+      <AutoComplete @result-selected="selectResult" />
+      <WeekView
+        :selectedStation="selectedStation"
+        :weekDays="weekDays"
+        :bookings="bookings"
+        @open-booking-details="openBookingDetails"
+      />
+      <BookingDetailModal
+        :selectedBooking="selectedBooking"
+        :selectedStation="selectedStation"
+        :bookings="bookings"
+        @close-booking-details="closeBookingDetails"
+      />
+    </div>
   </main>
 </template>
 
 <script>
-import autoComplete from './components/auto-complete.vue'
+import AutoComplete from './components/AutoComplete.vue'
+import WeekView from './components/WeekView.vue'
+import BookingDetailModal from './components/BookingDetailModal.vue'
+
 export default {
   components: {
-    autoComplete
+    AutoComplete,
+    WeekView,
+    BookingDetailModal
   },
+  data() {
+    return {
+      inputValue: '',
+      results: [],
+      showResults: false,
+      selectedStation: null,
+      bookings: [],
+      weekDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      selectedDay: null,
+      selectedBooking: null
+    }
+  },
+
   methods: {
-    handleSelectedSelected(selectedStation) {
-      console.log('Selected station:', selectedStation)
+    selectResult(result) {
+      this.selectedStation = result
+
+      const apiBooking = `https://605c94c36d85de00170da8b4.mockapi.io/stations/${result.id}/bookings`
+
+      fetch(apiBooking)
+        .then((response) => response.json())
+        .then((data) => {
+          this.bookings = data
+        })
+        .catch((error) => {
+          console.log('Error fetching booking:', error)
+        })
+
+      this.$emit('result-selected', result)
+      this.showResults = false
+      this.inputValue = ''
+    },
+    openBookingDetails(booking) {
+      this.selectedBooking = booking
+    },
+
+    closeBookingDetails() {
+      this.selectedBooking = null
     }
   }
 }
---
+++ src/components/AutoComplete.vue
@@ -0,0 +1,54 @@
+<template>
+  <form>
+    <label for="input" class="p-2 font-bold text-lg md:text-2xl">
+      Search for station:
+      <input
+        id="input"
+        v-model="inputValue"
+        @input="search"
+        class="font-normal border-2 rounded border-blue-700"
+      />
+    </label>
+    <div v-if="showResults" class="flex justify-center py-2">
+      <ul class="w-full md:w-1/5 mt-1 center border border-gray-300 rounded-md bg-white/60">
+        <li
+          v-for="result in results"
+          :key="result.id"
+          @click="selectResult(result)"
+          class="border-b border-gray-200 text-stone-600 cursor-pointer hover:bg-gray-100 transition-colors"
+        >
+          {{ result.name }}
+        </li>
+      </ul>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: '',
+      results: [],
+      showResults: false
+    }
+  },
+  methods: {
+    search() {
+      const api = `https://605c94c36d85de00170da8b4.mockapi.io/stations?search=${this.inputValue}`
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          this.results = data
+          this.showResults = true
+        })
+    },
+    selectResult(result) {
+      this.$emit('result-selected', result)
+      this.showResults = false
+      this.inputValue = ''
+      console.log('something was selected')
+    }
+  }
+}
+</script>
