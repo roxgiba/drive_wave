42cd8079b242fc5103eeaa5464d34dc389a63de3
roxgiba <rox.giba@gmail.com>
2024-02-09 10:52:00 +0100

    week view updating

--- src/components/WeekView.vue
+++ src/components/WeekView.vue
@@ -1,13 +1,17 @@
 <template>
   <div v-if="selectedStation" class="mt-10 bg-stone-300/80 p-3 rounded-lg">
-    <h2 class="text-xl md:text-2xl font-bold pb-2">Week View for: {{ selectedStation.name }}</h2>
-
+    <h2 class="text-lg md:text-2xl font-bold pb-2">
+      Week View for:
+      <span class="text-xl md:text-3xl tracking-wide underline">{{ selectedStation.name }}</span>
+    </h2>
+    <VCalendar view="weekly" transparent borderless locale="en-US" class="m-3" h-10 />
     <div id="calendar-like">
-      <div>
-        <button class="text-xs pr-5">&lt; previous</button>
-        <span class="font-bold">month & year</span>
-        <button class="text-xs pl-5">next ></button>
-      </div>
+      <!-- <div class="p-6">
+        <button class="text-xs pr-5" @click="prevMonth">&lt; previous</button>
+        <span class="font-bold">{{ currentMonthAndYear }}</span>
+        <button class="text-xs pl-5" @click="nextMonth">next ></button>
+      </div> -->
+
       <div id="week-view" class="m-8 grid grid-cols-1 md:grid-cols-7 gap-4">
         <div v-for="day in weekDays" :key="day" class="day-tile rounded-md">
           <h3 className="font-bold text-lg bg-white/50 ">{{ day }}</h3>
@@ -37,13 +41,13 @@ export default {
 
   data() {
     return {
-      selectedBooking: null // Add this line
+      selectedBooking: null,
+      currentDate: new Date(),
+      currentMonthAndYear: ''
     }
   },
 
   methods: {
-    // ... (Existing methods related to Week View)
-
     getBookingsForDay(day) {
       // Return the bookings for the selected day
       return this.bookings.filter(
@@ -56,6 +60,21 @@ export default {
       this.selectedBooking = booking
       console.log('opening modal')
     }
+
+    // updateMonthAndYear() {
+    //   const options = { month: 'long', year: 'numeric' }
+    //   this.currentMonthAndYear = this.currentDate.toLocaleDateString('en-GB', options)
+    // },
+
+    // prevMonth() {
+    //   this.currentDate.setMonth(this.currentDate.getMonth() - 1)
+    //   this.updateMonthAndYear()
+    // },
+
+    // nextMonth() {
+    //   this.currentDate.setMonth(this.currentDate.getMonth() + 1)
+    //   this.updateMonthAndYear()
+    // },
   }
 }
 </script>
